3003a6690dbca82bacb10380768200f1982dbe90
Joy Umali <joyumali@Joys-MacBook-Pro.local>
2016-12-14 22:42:20 -0800

    Center marker

--- MotoRangerGM/MapVC.swift
+++ MotoRangerGM/MapVC.swift
@@ -14,22 +14,25 @@ class MapVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Set center coordinates
+        // Setup mapView
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.mapType = kGMSTypeHybrid
         mapView.isMyLocationEnabled = true
         view = mapView
         
-        // Create a marker in the center of the map
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sidney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+    // Create a marker in the center of the map
+    let marker = GMSMarker()
+    marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+    marker.title = "Sidney"
+    marker.snippet = "Australia"
+    marker.map = mapView
+    
+    
     }
 
-
-
-
 }
+
+
+
 
