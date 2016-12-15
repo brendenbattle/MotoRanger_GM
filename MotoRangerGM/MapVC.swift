634d21361c233cdd4fcafe66b73d1344f8757d8b
Joy Umali <joyumali@Joys-MacBook-Pro.local>
2016-12-14 20:11:59 -0800

    points to Sidney

---
+++ MotoRangerGM/MapVC.swift
@@ -0,0 +1,35 @@
+//
+//  MapVC.swift
+//  MotoRangerGM
+//
+//  Created by Joy Umali on 12/14/16.
+//  Copyright © 2016 Joy Umali. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Set center coordinates
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        view = mapView
+        
+        // Create a marker in the center of the map
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sidney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+
+
+
+
+}
+
